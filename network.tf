52cb4b53654e4be2f83cbea5fabed3a457ea5534
jsr145 <102553702+jsr145@users.noreply.github.com>
2022-03-29 18:19:06 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -5,4 +5,5 @@ module "network" {
   source  = "app.terraform.io/JR-TEST/network/azurerm"
   version = "3.5.0"
   # insert required variables here
+  resource_group_name = "azurerm_resource_group.myresourcegroup.name"
 }
